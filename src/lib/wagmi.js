433fa4a9529130f5b2b18f958a497aa64dd33f57
Feranmi <89228157+Feranmi10@users.noreply.github.com>
2026-05-16 07:59:56 +0100

    Update wagmi.js

--- src/lib/wagmi.js
+++ src/lib/wagmi.js
@@ -14,7 +14,11 @@ export const wagmiConfig = createConfig({
 })
 
 export const CONTRACTS = {
-  pachiPay: "0xF5F89866997eA666B3FAa4a8F6Fca76CF66b0e54",
-  badge:    "0xD7116FDe599b06E4A2ca24BF88B8338677591C5a",
-  usdc:     "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  pachiPay:         "0xF5F89866997eA666B3FAa4a8F6Fca76CF66b0e54",
+  reputationBadge:  "0xD7116FDe599b06E4A2ca24BF88B8338677591C5a",
+  pachiToken:       "0xAcb3Bd9286330049c6e195e6527f3365E9c3030C",
+  pachiStaking:     "0x3De4A1Deb042189ff299221d3546Ea1a4aa06109",
+  pachiGovernance:  "0x7EE984447380857e22C2bc9beFa4D53f9B118ea3",
+  pachiMarketplace: "0xF0EC740FFe498463B3836Afa67Ce2b66AC7b6fDD",
+  usdc:             "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
 }
